11cb464ca28b8158058d6a847119d26bc8bfdd40
Vivien51380 <113355826+Vivien51380@users.noreply.github.com>
2022-12-16 10:34:28 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import Footer from "./components/Footer";
 import Deco from "@components/Decoration/Deco";
 import LoginSignupContainer from "@components/LoginSignupContainer/LoginSignupContainer";
 import NavBar from "@components/NavBar/NavBar";
 import Form from "./components/FormDecision";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
