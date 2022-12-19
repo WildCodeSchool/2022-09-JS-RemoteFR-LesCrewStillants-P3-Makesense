d6a71292ddb645467b0db793ef974047906d9557
lana duval <lanaduval09@gmail.com>
2022-12-19 20:44:27 +0100

    MAJ migrate.js to use make_sense db and create a fonctionnal GET road for table user

---
+++ backend/src/controllers/userControllers.js
@@ -0,0 +1,17 @@
+const models = require("../models");
+
+const browseUser = (req, res) => {
+  models.user
+    .findAll()
+    .then(([rows]) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  browseUser,
+};
--- backend/src/router.js
+++ backend/src/router.js
@@ -10,4 +10,16 @@ router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
 
+const userControllers = require("./controllers/userControllers");
+
+router.get("/users", userControllers.browseUser);
+router.get("/users/:id", userControllers.browseUser);
+/* router.put("/users/:id", userControllers.editUser);
+router.post("/users", userControllers.addUser);
+
+
+const decisionControllers = require("./controllers/decisionControllers");
+
+const commentControllers = require("./controllers/commentControllers"); */
+
 module.exports = router;
