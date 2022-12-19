a6a650bf291b0aa5c19b6fc787844c5fc2365b57
lana duval <lanaduval09@gmail.com>
2022-12-19 14:49:57 +0100

    add react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,16 @@
-import Deco from "@components/Decoration/Deco";
-import LoginSignupContainer from "@components/LoginSignupContainer/LoginSignupContainer";
-import NavBar from "@components/NavBar/NavBar";
-import Form from "./components/FormDecision";
-import Footer from "./components/Footer";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Decision from "./pages/PriseDecision";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">      
-      <NavBar />
-      <LoginSignupContainer />
-      <Deco />
-      <Form />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/decision" element={<Decision />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
