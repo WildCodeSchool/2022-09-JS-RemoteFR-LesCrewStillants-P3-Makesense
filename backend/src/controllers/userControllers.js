315f25f3d5b907a840a8b76ad6ae921fc4c4e540
lana duval <lanaduval09@gmail.com>
2022-12-20 10:54:52 +0100

    add a delete road for users

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -65,9 +65,26 @@ const addUser = (req, res) => {
     });
 };
 
+const destroyUser = (req, res) => {
+  models.user
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   browseUser,
   addUser,
   editUser,
   readUser,
+  destroyUser,
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -16,6 +16,7 @@ router.get("/users", userControllers.browseUser);
 router.get("/users/:id", userControllers.readUser);
 router.put("/users/:id", userControllers.editUser);
 router.post("/users", userControllers.addUser);
+router.delete("/users/:id", userControllers.destroyUser);
 /* 
 const decisionControllers = require("./controllers/decisionControllers");
 
