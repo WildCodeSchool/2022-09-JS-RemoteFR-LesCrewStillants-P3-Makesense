b1a2efa90a9efd1fbd4947d148d1f8fb4774d66c
QuentinVi <quentin.vi0510@gmail.com>
2023-01-04 10:32:49 +0100

    maj Decision routes2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import Form from "./components/FormDecision";
-import { DateContextProvider } from "./Context/DateContext";
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Decision from "./pages/PriseDecision";
 import "./App.css";
 
 function App() {
   return (
-    <DateContextProvider>
-      <div className="App">
-        <Form />
-      </div>
-    </DateContextProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/decision" element={<Decision />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/PriseDecision.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Form from "../components/FormDecision";
+import { DateContextProvider } from "../Context/DateContext";
+
+function PriseDecision() {
+  return (
+    <DateContextProvider>
+      <div className="PriseDecision">
+        <Link to="/">
+          <p className="home">Home</p>
+        </Link>
+        <Form />
+      </div>
+    </DateContextProvider>
+  );
+}
+
+export default PriseDecision;
