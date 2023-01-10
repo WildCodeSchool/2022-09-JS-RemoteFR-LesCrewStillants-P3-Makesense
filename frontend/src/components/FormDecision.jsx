9972f1889090bb032950fafcd4e888284a0ad09f
SEBASTIEN_ARTESI <SEBASTIEN.ARTESI@gmail.com>
2023-01-10 10:07:30 +0100

    création de component,.map date et onglet prise de décision

--- frontend/src/components/FormDecision.jsx
+++ frontend/src/components/FormDecision.jsx
@@ -1,22 +1,42 @@
 import { useState } from "react";
 import Footer from "./Footer";
-import "./FormD.css";
+import "./FormDecision.css";
 import Editor from "./RTE";
 import Timeline from "./Timeline";
+import DateStep from "./DateDecision";
 // import { DataContextProvider } from "../Context/DataContext";
+
+/* Créer usestate pour afficher/masquer onglet rte */
 
 function Form() {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState();
+  // const [titleDeadline, setTitleDeadLine] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
   };
+  const [stateOnglets, setStateOnglets] = useState(1);
+
+  const goDecision = () => {
+    setStateOnglets(1);
+  };
+  const goDate = () => {
+    setStateOnglets(2);
+  };
+
   const titles = [
     "#1 Les détails de la décision",
     "#2 Impact sur l'organisation",
     "#3 Bénéfices",
     "#4 Risques",
     "#5 Première Décision",
+  ];
+  const stepDeadlines = [
+    "#1 Prise de décision commencée",
+    "#2 Deadline pour donner son avis",
+    "#3 Première décision prise",
+    "#4 Deadline pour entrer en conflit",
+    "#5 Décision définitive",
   ];
   console.warn(date);
 
@@ -25,23 +45,61 @@ function Form() {
       <div className="Timeline">
         <Timeline />
       </div>
-      <form onSubmit={handleSubmit}>
-        <label className="Label">
-          <p className="labelF"> Insérez un titre pour votre décision </p>
-          <input
-            className="TitleD"
-            type="text"
-            placeholder="Titre décision "
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </label>
-        {titles.map((titleH2, id) => (
-          <Editor key={titleH2} setDate={setDate} id={id} title={titleH2} />
-        ))}
-        <br />
-        <input className="Submit" type="submit" value="Soumettre" />
-      </form>
+      <div>
+        <div className="contBtn">
+          <button
+            type="button"
+            onClick={goDecision}
+            className={`onglets ${stateOnglets === 1 ? "active" : ""}`}
+          >
+            Décision
+          </button>
+          <button
+            type="button"
+            onClick={goDate}
+            className={`onglets ${stateOnglets === 2 ? "active" : ""}`}
+          >
+            Date
+          </button>
+        </div>
+        <div className="container">
+          {stateOnglets === 1 ? (
+            <p className="contenu Decision">
+              <form onSubmit={handleSubmit}>
+                <label className="Label">
+                  <p className="labelF">
+                    {" "}
+                    Insérez un titre pour votre décision{" "}
+                  </p>
+                  <input
+                    className="TitleD"
+                    type="text"
+                    placeholder="Titre décision "
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </label>
+                {titles.map((titleH2, id) => (
+                  <Editor key={titleH2} id={id} title={titleH2} />
+                ))}
+                <br />
+                <input className="Submit" type="submit" value="Soumettre" />
+              </form>
+            </p>
+          ) : (
+            <p className="contenu Date">
+              {stepDeadlines.map((stepDeadlineH2, id) => (
+                <DateStep
+                  key={stepDeadlineH2}
+                  setDate={setDate}
+                  id={id}
+                  title={stepDeadlineH2}
+                />
+              ))}
+            </p>
+          )}
+        </div>
+      </div>
       <Footer />
     </>
   );
--- frontend/src/components/RTE.jsx
+++ frontend/src/components/RTE.jsx
@@ -1,28 +1,16 @@
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import React, { useContext } from "react";
-import { DateContext } from "../Context/DateContext";
 import { DataContext } from "../Context/DataContext";
 
 // eslint-disable-next-line react/prop-types
 function Editor({ id, title }) {
-  // permet de récupérer la date courante et de ne pas sélectionner une date antérieur
-  const currentDate = new Date().toISOString().split("T")[0];
-  const { nativeDate, handleDateChange } = useContext(DateContext);
   const { handleDataChange } = useContext(DataContext);
 
   return (
     <div>
+      {/* faire un component input date */}
       <h2>{title}</h2>
-      <input
-        className="date"
-        type="date"
-        name={title}
-        id={`form-${id}`}
-        min={currentDate}
-        value={nativeDate.title}
-        onChange={handleDateChange}
-      />
       <CKEditor
         editor={ClassicEditor}
         onChange={(event, editor) => {
