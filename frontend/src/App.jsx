0a5065ca95a89149596f5af1462531122eb35e15
MalikMch <mouchouumalik@gmail.com>
2023-01-26 17:26:33 +0100

    decision details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import Decision from "./pages/PriseDecision";
 import AccueilSalarie from "./pages/AccueilSalarie";
 import Admin from "./pages/Admin";
-
+import DecisionFinale from "./pages/DecisionFinale";
 import "./App.css";
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/decision" element={<Decision />} />
         <Route path="/accueil-salarie" element={<AccueilSalarie />} />
+        <Route path="/decisionfinale" element={<DecisionFinale />} />
         <Route path="/accueil-admin" element={<AccueilAdmin />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
---
+++ frontend/src/components/Accordion.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from "react";
+// eslint-disable-next-line react/prop-types
+function Accordion({ title }) {
+  const decisions = [
+    {
+      id: "4",
+      title: "Titre",
+      desc_final: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      status: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      user_id: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+  ];
+
+  const [active, setActive] = useState(false);
+  const handleToggle = () => {
+    setActive(!active);
+  };
+
+  return (
+    <div className="accordions">
+      {decisions.map((decision) => (
+        <div key={decision.id}>
+          <div className={`accordion ${active && "active"}`}>
+            <div className="accordion__title" onClick={handleToggle}>
+              {title} <div className="accordion__icon" />
+            </div>
+            <div className="accordion__content">{decision.desc_final}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Accordion;
